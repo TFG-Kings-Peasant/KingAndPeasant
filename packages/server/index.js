0e0f0a116f74d62fcba36ab872b1a97f88b6beda
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-01-11 19:55:15 +0100

    fix: redis docker working

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
+import express from 'express';
+import { connectDB } from './config/db';
+import { connectRedis } from './config/redis';
 
 const app = express();
 const port = 3000;
+
+connectDB();
+connectRedis();
+
 app.get("/api", (req, res) => { //.get only responds to HTTP GET requests
     res.send("Hello World!");
-});
-
-app.get("/api/try", (req,res) => {
-    res.json({message: "Prueba exitosa"});
 });
 
 app.get("/api/data", async (req, res) => {
