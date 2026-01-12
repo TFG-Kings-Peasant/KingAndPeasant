7d5a9fd70c73825589e13116935bc3b472402663
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-01-12 21:10:40 +0100

    feat: First Connection with mariaDB and redis

--- packages/server/config/db.js
+++ packages/server/config/db.js
@@ -1,6 +1,8 @@
 import mariadb from "mariadb";
 
-const pool = mariadb.createPool({
+let createUserQuery = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) UNIQUE, password VARCHAR(255))";
+
+export const pool = mariadb.createPool({
     host: 'mariadb',
     user: 'user',
     password: 'password',
@@ -11,11 +13,21 @@ export async function connectDB() {
     let conn; 
     try {
         conn = await pool.getConnection();
+        await runQuery(conn, createUserQuery);
         console.log("Connected to MariaDB!");
     } catch (err) {
         console.error("Error conectando a MariaDB:", err);
     } finally {
         if (conn) conn.release();
+
+    }
+}
+
+async function runQuery(connection, query) {
+    try {
+        await connection.query(query);
+    } catch (err) {
+        console.error("Error queryng MariaDB:", err);
     }
 }
 
--- packages/server/index.js
+++ packages/server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { connectDB } from './config/db';
-import { connectRedis } from './config/redis';
+import { connectDB, pool } from './config/db.js';
+import { connectRedis, redisClient } from './config/redis.js';
 
 const app = express();
 const port = 3000;
@@ -8,12 +8,30 @@ const port = 3000;
 connectDB();
 connectRedis();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.get("/api", (req, res) => { //.get only responds to HTTP GET requests
     res.send("Hello World!");
 });
 
-app.get("/api/data", async (req, res) => {
-    
+app.post("/api/create-user", async (req, res) => {
+    const {username, password} = req.body;
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const result = await conn.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password]);
+        console.log(result);
+        await redisClient.set('user_created', JSON.stringify({userId: Number(result.insertId), username: username}));
+        res.status(201).send({message: "User created", userId: result.insertId.toString()});
+    } catch (err) {
+        console.error("Error creating user:", err);
+        res.status(500).send({message: "Error creating user"}); 
+        //Comprobar si esto lo tengo que poner asi con el catch, 
+        //console log y status 500
+    } finally {
+        if (conn) conn.release();
+    }
 });
 
 app.listen(port, () => {
