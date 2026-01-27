214cebf7fc3d550873eb7fb331af43c8cba1b017
Guillermo García León <guigarleo@alum.us.es>
2026-01-27 11:46:48 +0100

    style: Added style to Lobby List page
    Refs: #11

--- packages/client/src/pages/lobbyList/LobbyList.tsx
+++ packages/client/src/pages/lobbyList/LobbyList.tsx
@@ -1,34 +1,41 @@
 import Header from "../home/components/Header";
+import './LobbyList.css'
 
-
+const mockLobbies = [
+  { id: 1, name: "Sala de Principiantes", host: "HostUser_01", players: "1/2", status: "UNIRSE", type: "join" },
+  { id: 2, name: "Partida Seria", host: "ProGamer", players: "1/2", status: "PRIVADA", type: "private" },
+  { id: 3, name: "Test Room", host: "Dev", players: "2/2", status: "LLENA", type: "full" },
+];
 
 function LobbyList() {
     return <div>
         <Header username="Guille"/>
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Anfitrion</th>
-            <th>Jugadores</th>
-            <th>Estado</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>12345</td>
-            <td>HostUser_01</td>
-            <td>1/2</td>
-            <td>Unirse</td>
-        </tr>
-        <tr>
-            <td>67890</td>
-            <td>ProGamer</td>
-            <td>1/2</td>
-            <td>Privada</td>
-        </tr>
-    </tbody>
-</table>
+        <div className="body-container">
+            <div className="button-container">
+                <button>Crear Sala</button>
+                <button>Refrescar</button>
+            </div>
+            <div className="lobby-table">
+                <div className="table-header">
+                    <span>NOMBRE DE LA SALA</span>
+                    <span>ANFITRIÓN</span>
+                    <span>JUGADORES</span>
+                    <span>ESTADO</span>
+                </div>
+                <div className="table-body">
+                    {mockLobbies.map((lobby) => (
+                        <div key={lobby.id} className="table-row">
+                            <span className="col-name">{lobby.name}</span>
+                            <span className="col-host">{lobby.host}</span>
+                            <span className="col-players">{lobby.players}</span>
+                            <span className="col-status">
+                                <button className={`status-btn ${lobby.type}`}>{lobby.status}</button>
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     </div>;
 }
 
