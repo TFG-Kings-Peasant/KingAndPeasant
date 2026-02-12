9e13a7368adf12b14efef939589dade31d417f43
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-02-12 16:25:27 +0100

    fix: AuthContext

--- packages/client/src/context/AuthContext.tsx
+++ packages/client/src/context/AuthContext.tsx
@@ -1,33 +1,18 @@
-import { createContext, useState } from "react";
+import { useState } from "react";
 import type { ReactNode } from "react";
 import { useLocalStorage} from "../hooks/useLocalStorage";
-
-const { getItem, setItem, removeItem } = useLocalStorage();
-
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-    authToken?: string;
-}
-
-interface AuthContextType {
-  user: User | null;
-  isLogin: boolean;
-  login: ( userData: User ) => void;
-  logout: () => void;
-}
-
-export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+import type { User } from "./AuthContext.ts"
+import { AuthContext } from "./AuthContext.ts"
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
+    const { getItem, setItem, removeItem } = useLocalStorage();
     const [user, setUser] = useState<User | null>(() => {
         try {
-            const savedUser = localStorage.getItem("user");
+            const savedUser = getItem("user");
             return savedUser ? JSON.parse(savedUser) : null;
         } catch (error) {
             console.error("Error leyendo localStorage:", error);
