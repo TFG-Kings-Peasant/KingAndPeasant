33f3272b47f1d4ed0fe1cd9efca9413e99c9b86b
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-02-13 10:06:30 +0100

    fix: AuthProvider

--- packages/client/src/context/AuthProvider.tsx
+++ packages/client/src/context/AuthProvider.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { ReactNode } from "react";
 import { useLocalStorage} from "../hooks/useLocalStorage.ts";
 import type { User } from "./AuthContext.ts"
 import { AuthContext } from "./AuthContext.ts"
+import { io, Socket } from "socket.io-client"; 
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -10,6 +11,7 @@ interface AuthProviderProps {
 
 export function AuthProvider({ children }: AuthProviderProps) {
     const { getItem, setItem, removeItem } = useLocalStorage();
+    
     const [user, setUser] = useState<User | null>(() => {
         try {
             const savedUser = getItem("user");
@@ -19,9 +21,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
             return null;
         }
     });
-    const [isLogin, setIsLogin] = useState<boolean>(!!user);
-
     
+    const [isLogin, setIsLogin] = useState<boolean>(!!user);
+    const [socket, setSocket] = useState<Socket | null>(null); 
+    
+    useEffect(() => {
+        if (!user) 
+    }, [user]);
 
     function login( userData: User) {
         setUser(userData);
@@ -35,7 +41,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         removeItem("user");
     }
 
-    const value = { user, isLogin, login, logout };
+    const value = { user, isLogin, login, logout, socket };
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
