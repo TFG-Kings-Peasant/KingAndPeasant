ef2a5fe4575259818eda089c8778632c9d87bd39
GuilleGL <guillegarcialeon12@gmail.com>
2026-02-28 12:26:50 +0100

    fix: Fixed client url syntax error at in index.js

--- packages/server/index.js
+++ packages/server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: rocess.env.CLIENT_URL,
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST"]
     }
 });
