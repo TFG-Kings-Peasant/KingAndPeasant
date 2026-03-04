64708d3371df39040bca16ae4b8751e365c8f0fb
15M4C4R <mkhazni2004@gmail.com>
2026-03-04 18:50:36 +0100

    feat: resolve pending action in Controller

--- packages/server/src/controllers/GameController.js
+++ packages/server/src/controllers/GameController.js
@@ -6,7 +6,7 @@ const createGame = async (req, res) => {
         const lobbyId = req.params.id;
         const { player1Id, player2Id } = req.body;
         const {dtoKing, dtoPeasant} = await gameService.createGame(lobbyId, player1Id, player2Id);
-        res.status(201).json(dtoKing, dtoPeasant);
+        res.status(201).json({dtoKing, dtoPeasant});
     }catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -21,7 +21,7 @@ const getGameStatus = async (req, res) => {
 
         if (!dtoKing || !dtoPeasant) return res.status(404).send("Juego no encontrado");
 
-        const gameState= userId === dtoKing.players.king.id ? dtoKing : userId === dtoPeasant.players.peasant.id ? dtoPeasant : null;
+        const gameState = userId === dtoKing.players.king.id ? dtoKing : userId === dtoPeasant.players.peasant.id ? dtoPeasant : null;
 
         res.status(201).json(gameState);
         
@@ -38,12 +38,8 @@ const playCard = async (req, res) => {
 
         const {dtoKing, dtoPeasant} = await gameService.playCard(lobbyId, cardUid, targetData, userId);
     
-        if (userSockets[dtoKing.players.king.id]) {
-            io.to(userSockets[dtoKing.players.king.id]).emit('gameState', dtoKing);
-        }
-        if (userSockets[dtoPeasant.players.peasant.id]) {
-            io.to(userSockets[dtoPeasant.players.peasant.id]).emit('gameState', dtoPeasant);
-        }
+        sendGameStateUpdate(dtoKing, dtoPeasant);
+
         res.status(200).json({ message: "Carta jugada correctamente" });
     } catch (error) {
         console.error("Error al jugar la carta:", error.message);
@@ -51,7 +47,34 @@ const playCard = async (req, res) => {
     }
 }
 
+function sendGameStateUpdate (dtoKing, dtoPeasant) {
+    if (userSockets[dtoKing.players.king.id]) {
+        io.to(userSockets[dtoKing.players.king.id]).emit('gameState', dtoKing);
+    }
+    if (userSockets[dtoPeasant.players.peasant.id]) {
+        io.to(userSockets[dtoPeasant.players.peasant.id]).emit('gameState', dtoPeasant);
+    }
+}
+
+const resolveAction = async (req, res) => {
+    try {
+        const lobbyId = req.params.id;
+        const { cardUid, targetData } = req.body;
+        const userId  = Number(req.user.idUser);
+        const {dtoKing, dtoPeasant} = await gameService.resolvePendingAction(lobbyId, userId, targetData);
+    
+        sendGameStateUpdate(dtoKing, dtoPeasant);
+
+        res.status(200).json({ message: "Acción resuelta correctamente" });
+    } catch (error) {
+        console.error("Error al resolver la acción:", error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
+
 export const gameController = {
     createGame,
-    getGameStatus
+    getGameStatus,
+    playCard,
+    resolveAction
 }
--- packages/server/src/routes/GameRoutes.js
+++ packages/server/src/routes/GameRoutes.js
@@ -3,7 +3,9 @@ const router = Router();
 import { gameController } from '../controllers/GameController.js';
 
 router.post('/start', gameController.createGame);
-router.get('/:id', gameController.getGameStateById);
-router.post('/:id/playCard', gameController.exampleAction);
+router.get('/:id', gameController.getGameStatus);
+router.post('/:id/playCard', gameController.playCard);
+router.post('/:id/resolveAction', gameController.resolveAction);
+
 
 export default router;
