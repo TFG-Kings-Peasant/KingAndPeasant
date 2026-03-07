c7a5210e594c6b4794411f5ff1691659e20e1ca8
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-03-07 20:40:23 +0100

    fix: seeder

--- packages/server/prisma/seeds/seedCards.js
+++ packages/server/prisma/seeds/seedCards.js
@@ -17,12 +17,12 @@ export async function seedCards(prisma) {
     { nameKing: "Noble", typeKing: "Guard", descKing: "Reveal all Rebels", namePeasant: "Mob", typePeasant: "Rebel", descPeasant: "Remove all Rebels and Guards", copies: 1 },
     { nameKing: "Guardian", typeKing: "Guard", descKing: "Look at any 1 card in the deck, if it is the ASSASSIN discard it, otherwise put it back in order", namePeasant: "Charlatan", typePeasant: "Rebel", descPeasant: "Draw up to 3 cards, then put the same number of cards on top of the deck in any order", copies: 1 },
     { nameKing: "Executor", typeKing: "Guard", descKing: "Peasant Removes 1 hidden Rebel", namePeasant: "Rat", typePeasant: "Rebel", descPeasant: "Return up to 2 other Rebels back to hand, then Hide up to 2 Rebels", copies: 1 },
-    { nameKing: "Sentinel", typeKing: "Guard", descKing: "Look at the top 3 cards of the deck, then put them back in any order", namePeasant: "Exile", typePeasant: "None", descPeasant: "Exile", copies: 1 },
+    { nameKing: "Sentinel", typeKing: "Guard", descKing: "Look at the top 3 cards of the deck, then put them back in any order", namePeasant: "None", typePeasant: "None", descPeasant: "None", copies: 1 },
     { nameKing: "Strike", typeKing: "Action", descKing: "Mobilize 2 Guards", namePeasant: "Thief", typePeasant: "Rebel", descPeasant: "King discards 2 cards, then Peasant takes 1 of them", copies: 1 },
     { nameKing: "Arrest", typeKing: "Action", descKing: "Remove a Rebel or discard the top card of the deck", namePeasant: "Revolt", typePeasant: "Action", descPeasant: "Infiltrate and Dispatch all Rebels", copies: 1 },
     { nameKing: "Raid", typeKing: "Action", descKing: "Peasant discards 1 card at random", namePeasant: "Scatter", typePeasant: "Action", descPeasant: "Return all Rebels in Town back to hand, then shuffle the discard pile back into the deck", copies: 1 },
     { nameKing: "None", typeKing: "None", descKing: "-", namePeasant: "Decoy", typePeasant: "Rebel", descPeasant: "Infiltrate: Peasant removes a Guard. EXILE", copies: 1 },
-    { nameKing: "Reassemble", typeKing: "Action", descKing: "Take up to 2 cards from the discard pile, then Hide up to 1 Rebel or Ready up to 1 Guard", namePeasant: "Rally", typePeasant: "Action", descPeasant: "Draw 2 cards, then Hide up to 2 Rebels", copies: 1 },
+    { nameKing: "Reassemble", typeKing: "Action", descKing: "Take up to 2 cards from the discard pile, then Ready up to 1 Guard", namePeasant: "Reassemble", typePeasant: "Action", descPeasant: "Draw 2 cards, then Hide up to 2 Rebels", copies: 1 },
     { nameKing: "Watchman", typeKing: "Guard", descKing: "Look at Peasant's hand cards", namePeasant: "Rally", typePeasant: "Action", descPeasant: "Draw 2 cards, then Hide up to 2 Rebels", copies: 1 },
     { nameKing: "None", typeKing: "None", descKing: "-", namePeasant: "Assassin", typePeasant: "Rebel", descPeasant: "Infiltrate: Peasant wins. Dispatch: Peasant wins if there are no Guards in Town", copies: 1 }
   ];
